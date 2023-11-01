97b96a67ed27859cddb84b48147ab8eece0aaaa2
CartenziaRumbino <149613382+CartenziaRumbino@users.noreply.github.com>
2023-11-02 03:06:41 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState} from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const a=987
+  const b=354
+  return (
+    a % b
+  )
+}
+
+export default App
---
+++ home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+function home() {
+  return (
+    <div>
+    <App />
+    </div>
+  )
+}
+
+export default home
